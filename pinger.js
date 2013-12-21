b66cb9d79c7f34d12fe01fbf195fbc8c4309c182
Vaibhav <vaibhavgupta_iitd@yahoo.co.in>
2013-12-21 21:37:13 +0530

    Add files for javascript ping

---
+++ pinger.js
@@ -0,0 +1,84 @@
+$(function () {
+    
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
+    
+        var chart;
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container',
+                type: 'line',
+                marginRight: 10,
+                events: {
+                    load: function() {
+                        var series = this.series[0];
+                        setInterval(function() {
+                            var x = (new Date()).getTime();
+                            var startTime = (new Date()).getTime();
+                            var img = new Image();
+                            img.onload = function(){
+                                var responseTime = (new Date()).getTime() - startTime;
+                                console.log(responseTime);
+                                series.addPoint([x, responseTime], true);
+                              };
+                            img.onerror = function(){
+                                console.log("in error");
+                                series.addPoint([x, 0], true);
+                            };
+                            img.src = "https://lh3.googleusercontent.com/-exdVIn73GqA/AAAAAAAAAAI/AAAAAAAAAAA/BwCer9DYe3I/s30-c/photo.jpg?cachebreaker="+new Date().getTime();
+                            
+                        }, 1000);
+                    }
+                }
+            },
+            title: {
+                text: 'Live random data'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function() {
+                        return '<b>'+ this.series.name +'</b><br/>'+
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                        Highcharts.numberFormat(this.y, 2);
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            series: [{
+                name: 'Random data',
+                data: []
+            }]
+        });
+});
+
+function Pinger_ping(ip, callback) {
+
+  var startTime = (new Date()).getTime();
+  var responseTime = 0;
+  var img = new Image();
+  img.onload = function(){
+    responseTime = (new Date()).getTime() - startTime;
+  };
+  img.src = "http://"+ip;
+  return responseTime;
+}
